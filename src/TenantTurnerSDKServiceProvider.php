ef69bf24ea8780994ab5634d9fb8eb89aeb22366
ArturOlar <a.olar@tenantcloud.com>
2025-03-27 16:23:59 +0200

    feat: Added logging

--- src/TenantTurnerSDKServiceProvider.php
+++ src/TenantTurnerSDKServiceProvider.php
@@ -4,6 +4,7 @@ namespace TenantCloud\TenantTurner;
 
 use Illuminate\Config\Repository;
 use Illuminate\Support\ServiceProvider;
+use Psr\Log\LoggerInterface;
 use TenantCloud\TenantTurner\Client\TenantTurnerClient;
 use TenantCloud\TenantTurner\Client\TenantTurnerClientImpl;
 use TenantCloud\TenantTurner\Fake\FakeTenantTurnerClient;
@@ -29,11 +30,14 @@ final class TenantTurnerSDKServiceProvider extends ServiceProvider
 	private function bindDefaultClient(): void
 	{
 		$config = $this->app->make(Repository::class);
+		$logger = $this->app->make(LoggerInterface::class);
 
 		if (!$config->get('tenant_turner.fake_client')) {
 			$this->app->bind(TenantTurnerClient::class, fn () => new TenantTurnerClientImpl(
 				$config->get('tenant_turner.api_key'),
 				$config->get('tenant_turner.base_url'),
+				30,
+				$logger,
 			));
 		} else {
 			$this->app->bind(TenantTurnerClient::class, FakeTenantTurnerClient::class);
